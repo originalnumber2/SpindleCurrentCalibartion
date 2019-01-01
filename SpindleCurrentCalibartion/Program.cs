119d024fac15b171c444544762b66eefb862786f
Unknown <adam.jarrell0720@gmail.com>
2019-01-01 12:50:15 -0600

    Added Further Logging Capabilites

--- SpindleCurrentCalibartion/Program.cs
+++ SpindleCurrentCalibartion/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SpindleCurrentCalibartion
 {
@@ -16,24 +12,27 @@ namespace SpindleCurrentCalibartion
         static Dyno dynomometer;
         //static Random random = new Random();
         static int DataPoints = 1000;
+        static int SampleDelay = 10;
         static double[] Speeds = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800 };
         //static double[] Speeds = {100, 200, 300, 400, 500};
+        static string path = "Data_" + DateTime.Now.ToString("yyyy-MM-dd hh-mm") + "/";
 
         static void Main(string[] args)
         {
-            double[] Averages = new double[4];
+            double[] Averages = new double[5];
 
             SetUp();
             for (int i = 0; i < Speeds.Length; i++)
             {
                 Controller.StartSpindle(Speeds[i], true);
-                Thread.Sleep(1000);
+                Thread.Sleep(10000);
                 //Writter.AddDoubleArray(data);
                 double[][] data = TakeData();
-                Averages[0] = Average.GetAverage(data[0]);
-                Averages[1] = Average.GetAverage(data[1]);
-                Averages[2] = Averages[0] * 230;
-                Averages[3] = Averages[1] * Speeds[i] * Math.PI / 30;
+                Averages[0] = Speeds[i];
+                Averages[1] = Average.GetAverage(data[0]);
+                Averages[2] = Average.GetAverage(data[1]);
+                Averages[3] = Averages[0] * 230;
+                Averages[4] = Averages[1] * Speeds[i] * Math.PI / 30;
                 Controller.StartSpindle(0, true);
                 Console.WriteLine(Averages[0].ToString() + ", " + Averages[1].ToString() + ", " + Averages[2].ToString() + ", " + Averages[3].ToString());
                 Writter.AddDoubleArray(Averages);
@@ -46,9 +45,11 @@ namespace SpindleCurrentCalibartion
             Controller.StopSpindle();
         }
 
-        private static double[][] TakeData()
+        private static double[][] TakeData(int RPM)
         {
-            
+            CsvWriter csvWriter = new CsvWriter(",", path + "RPM" + RPM);
+            string[] header = { "Current", "Torque" };
+            csvWriter.WriteHeader(header);
             double[][] data = new double[2][];
             data[0] = new double[DataPoints];
             data[1] = new double[DataPoints];
@@ -57,9 +58,12 @@ namespace SpindleCurrentCalibartion
             {
                 double[] tempCurrentData = NIDaq.ReadUSBData();
                 double[] tempTorqueData = dynomometer.ReadDyno();
+                double[] tempWriterData = { tempCurrentData[1], tempTorqueData[3] };
+                csvWriter.AddDoubleArray(tempWriterData);
                 data[0][i] = tempCurrentData[1];
                 data[1][i] = tempTorqueData[3];
-                Thread.Sleep(10);
+
+                Thread.Sleep(SampleDelay);
                 //data[i] = random.NextDouble();
             }
             return data;
@@ -70,14 +74,16 @@ namespace SpindleCurrentCalibartion
             NIDaq = new NIDaqStrain();
             Controller = new MotorController();
             dynomometer = new Dyno();
-            Writter = new CsvWriter(",", "Spindle.csv");
+            Writter = new CsvWriter(",", path+"Spindle.csv");
             Average = new Averager();
             Controller.ConnSpindle();
             dynomometer.DynoConnect();
             dynomometer.DynoReset();
             Thread.Sleep(500);
-            String[] header = { "Current", "Torque", "Power (Current)", "Power (Torque)" };
-            Writter.WriteHeader(header);
+            String[] header1 = { "DataPoints:", DataPoints.ToString(), "SampleDelay:", SampleDelay.ToString() };
+            String[] header2 = { "RPM", "Current", "Torque", "Power (Current)", "Power (Torque)" };
+            Writter.WriteHeader(header1);
+            Writter.WriteHeader(header2);
         }
 
     }
